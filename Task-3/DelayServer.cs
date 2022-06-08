76d63720ada017321df7d858d1d114ef4e8b7426
Cobret <konstantin.britikov@gmail.com>
2022-06-08 20:40:11 +0200

    Init commit

---
+++ Task-3/DelayServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace Task_3
+{
+    public class DelayServer
+    {
+        public delegate void AnswerDelegate(string text);
+        public event AnswerDelegate Answer;
+
+        private string _answer;
+        private Timer _timer;
+
+        public void ReverseString(string text)
+        {
+            char[] chars = text.ToCharArray();
+            Array.Reverse(chars);
+            _answer = new string(chars);
+            SetTimer();
+        }
+        public void CapitilizeString(string text)
+        {
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+                chars[i] = char.ToUpper(chars[i]);
+            _answer = new string(chars);
+            SetTimer();
+        }
+        public void SplitString(string text)
+        {
+            char[] chars = text.ToCharArray();
+            List<string> list = new List<string>();
+            foreach(char c in chars)
+            {
+                list.Add(c.ToString());
+                list.Add(" ");
+            }
+            string result = string.Empty;
+            foreach (string s in list) result += s;
+            _answer = result;
+            SetTimer();
+        }
+        private void SetTimer()
+        {
+            Random random = new Random();
+            int interval = random.Next(5, 15) * 1000;
+            _timer = new Timer(interval);
+            _timer.AutoReset = false;
+            _timer.Elapsed += ReturnAnswer;
+            _timer.Enabled = true;
+
+            _timer.Start();
+        }
+        private void ReturnAnswer(object sender, ElapsedEventArgs e)
+        {
+            Answer?.Invoke(_answer);
+        }
+    }
+}
---
+++ Task-3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace Task_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TASK#3");
+            ChooseOperation();
+        }
+        static void ChooseOperation()
+        {
+            Console.WriteLine();
+
+            Dictionary<string, Action> dictionary = new Dictionary<string, Action>()
+            {
+                {"Reverse string",  ReverseString},
+                {"Capitilize string",  CapitilizeString},
+                {"Split string",  SplitString},
+            };
+            for (int i = 0; i < dictionary.Count; i++)
+                Console.WriteLine($"{i}. {dictionary.ElementAt(i).Key}");
+
+            Console.Write($"\nType index of operation ({0} - {dictionary.Count - 1}): ");
+            string userAnswer = Console.ReadLine();
+            int indexOfOperation = 0;
+
+            if (string.IsNullOrWhiteSpace(userAnswer) 
+                || !int.TryParse(userAnswer, out indexOfOperation)
+                || (indexOfOperation >= dictionary.Count || indexOfOperation < 0))
+            {
+                Console.WriteLine("\nERROR");
+                ChooseOperation();
+            }
+
+            dictionary.ElementAt(indexOfOperation).Value.Invoke();
+        }
+
+        static void ReverseString()
+        {
+            Console.Write("\n\nType a string for reverse: ");
+            string userAnswer = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userAnswer))
+            {
+                Console.WriteLine("\nERROR");
+                ReverseString();
+            }
+
+            server = new DelayServer();
+            server.Answer += DisplayAnswer;
+            server.ReverseString(userAnswer);
+            EnableLoading();
+        }
+        static void CapitilizeString()
+        {
+            Console.Write("\n\nType a string for capitilize: ");
+            string userAnswer = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userAnswer))
+            {
+                Console.WriteLine("\nERROR");
+                CapitilizeString();
+            }
+
+            server = new DelayServer();
+            server.Answer += DisplayAnswer;
+            server.CapitilizeString(userAnswer);
+            EnableLoading();
+        }
+        static void SplitString()
+        {
+            Console.Write("\n\nType a string for split: ");
+            string userAnswer = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userAnswer))
+            {
+                Console.WriteLine("\nERROR");
+                SplitString();
+            }
+
+            server = new DelayServer();
+            server.Answer += DisplayAnswer;
+            server.SplitString(userAnswer);
+            EnableLoading();
+        }
+        static void DisplayAnswer(string text)
+        {
+            Console.Write($" {text}");
+            LoadingTimer.Stop();
+            Console.ReadLine();
+        }
+        static Timer LoadingTimer;
+        static DelayServer server;
+        static void EnableLoading()
+        {
+            LoadingTimer = new Timer(500);
+            LoadingTimer.AutoReset = true;
+            LoadingTimer.Enabled = true;
+            LoadingTimer.Elapsed += ShowLoadDots;
+            PauseConsole();
+        }
+        static void PauseConsole()
+        {
+            while (Console.ReadKey(true).Key == ConsoleKey.Q) StopConsole();
+        }
+        static void ShowLoadDots(object sender, ElapsedEventArgs e)
+        {
+            Console.Write(" .");
+        }
+        static void StopConsole()
+        {
+            LoadingTimer.Stop();
+            server.Answer -= DisplayAnswer;
+            Console.WriteLine("\nSTOP");
+        }
+    }
+}
